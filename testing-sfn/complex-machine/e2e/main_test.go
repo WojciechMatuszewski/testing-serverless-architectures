e63dac5eea02da27d3fb62927edef2022ecb0253
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2022-12-29 14:07:15 +0100

    finish sfn complex

--- testing-sfn/complex-machine/e2e/main_test.go
+++ testing-sfn/complex-machine/e2e/main_test.go
@@ -11,6 +11,9 @@ import (
 	"github.com/avast/retry-go"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	dynamodbattributevalue "github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	dynamodbtypes "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/service/sfn"
 	sfntypes "github.com/aws/aws-sdk-go-v2/service/sfn/types"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
@@ -31,7 +34,6 @@ func TestWorkflow(t *testing.T) {
 	stateMachineArn := os.Getenv("OrdersMachineArn")
 	ctx := context.Background()
 
-	// TODO: check in DDB
 	t.Run("Order accepted flow", func(t *testing.T) {
 		stopPollingForOrderedOrders, orderedOrders := pollForOrderMessages(ctx, os.Getenv("OrderedOrdersQueueUrl"))
 		stopPollingForAcceptedOrders, acceptedOrders := pollForOrderMessages(ctx, os.Getenv("AcceptedOrdersQueueUrl"))
@@ -41,6 +43,8 @@ func TestWorkflow(t *testing.T) {
 			panic(fmt.Errorf("Could not load config: %w", err))
 		}
 
+		dynamoDBClient := dynamodb.NewFromConfig(config)
+
 		orderId := uuid.New().String()
 		sfnClient := sfn.NewFromConfig(config)
 		out, err := sfnClient.StartExecution(ctx, &sfn.StartExecutionInput{
@@ -48,16 +52,37 @@ func TestWorkflow(t *testing.T) {
 			Input:           aws.String(fmt.Sprintf("{\"orderId\": \"%s\"}", orderId)),
 		})
 		if err != nil {
-			panic(fmt.Errorf("Could not start execution: %w", err))
+			t.Fatal(fmt.Errorf("Could not start execution: %w", err))
 		}
 		executionArn := out.ExecutionArn
 
-		for orderedOrders := range orderedOrders {
-			for _, orderedOrder := range orderedOrders {
+		fmt.Println("Order ordered! checking in the database")
+
+		err = retry.Do(func() error {
+			order, err := readOrder(ctx, dynamoDBClient, orderId)
+			if err != nil {
+				return err
+			}
+			if order.Status != "ORDERED" {
+				return fmt.Errorf("Order is not ordered: %s", order.Status)
+			}
+			return nil
+		}, retry.Attempts(5), retry.Delay(500*time.Millisecond))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Println("Order ordered in the database! Checking the messages")
+
+		for currentOrderedOrders := range orderedOrders {
+			for _, orderedOrder := range currentOrderedOrders {
+				fmt.Println("Current ordered orders", currentOrderedOrders)
+
 				if orderedOrder.OrderId != orderId {
 					continue
 				}
 
+				fmt.Println("Accepting the order", orderId)
 				_, err = sfnClient.SendTaskSuccess(ctx, &sfn.SendTaskSuccessInput{
 					Output: aws.String(
 						fmt.Sprintf("{\"orderId\": \"%s\", \"accepted\": true}", orderedOrder.OrderId),
@@ -65,20 +90,22 @@ func TestWorkflow(t *testing.T) {
 					TaskToken: aws.String(orderedOrder.TaskToken),
 				})
 				if err != nil {
-					panic(fmt.Errorf("Could not send task success: %w", err))
+					t.Fatal(fmt.Errorf("Could not send task success: %w", err))
 				}
 
 				stopPollingForOrderedOrders()
 			}
 		}
 
-		fmt.Println("Order accepted! Now waiting for the fulfillment")
+		fmt.Println("Order ordered in the database! Waiting for acceptation")
 
-		for acceptedOrders := range acceptedOrders {
-			for _, acceptedOrder := range acceptedOrders {
+		for currentAcceptedOrders := range acceptedOrders {
+			for _, acceptedOrder := range currentAcceptedOrders {
 				if acceptedOrder.OrderId != orderId {
 					continue
 				}
+
+				fmt.Println("Confirming order acceptation", orderId)
 				_, err = sfnClient.SendTaskSuccess(ctx, &sfn.SendTaskSuccessInput{
 					Output: aws.String(
 						fmt.Sprintf("{\"orderId\": \"%s\"}", acceptedOrder.OrderId),
@@ -86,14 +113,30 @@ func TestWorkflow(t *testing.T) {
 					TaskToken: aws.String(acceptedOrder.TaskToken),
 				})
 				if err != nil {
-					panic(fmt.Errorf("Could not send task success: %w", err))
+					t.Fatal(fmt.Errorf("Could not send task success: %w", err))
 				}
 
 				stopPollingForAcceptedOrders()
 			}
 		}
 
-		fmt.Println("Order fulfilled! Checking the execution status")
+		fmt.Println("Order accepted! checking in the database")
+
+		err = retry.Do(func() error {
+			order, err := readOrder(ctx, dynamoDBClient, orderId)
+			if err != nil {
+				return err
+			}
+			if order.Status != "ACCEPTED" {
+				return fmt.Errorf("Order is not accepted: %s", order.Status)
+			}
+			return nil
+		}, retry.Attempts(5), retry.Delay(500*time.Millisecond))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Println("Order accepted in the database! Checking execution status")
 
 		err = retry.Do(func() error {
 			out, err := sfnClient.DescribeExecution(ctx, &sfn.DescribeExecutionInput{
@@ -110,10 +153,124 @@ func TestWorkflow(t *testing.T) {
 			return nil
 		}, retry.Attempts(5), retry.Delay(1*time.Second))
 		if err != nil {
-			panic(fmt.Errorf("Could not describe execution: %w", err))
+			t.Fatal(fmt.Errorf("Could not describe execution: %w", err))
 		}
 
-		fmt.Println("All done!")
+	})
+
+	t.Run("Order rejected flow", func(t *testing.T) {
+		stopPollingForOrderedOrders, orderedOrders := pollForOrderMessages(ctx, os.Getenv("OrderedOrdersQueueUrl"))
+		stopPollingForRejectedOrders, rejectedOrders := pollForOrderMessages(ctx, os.Getenv("RejectedOrdersQueueUrl"))
+
+		config, err := config.LoadDefaultConfig(ctx)
+		if err != nil {
+			panic(fmt.Errorf("Could not load config: %w", err))
+		}
+
+		dynamoDBClient := dynamodb.NewFromConfig(config)
+
+		orderId := uuid.New().String()
+		fmt.Println("OrderId", orderId)
+
+		sfnClient := sfn.NewFromConfig(config)
+		out, err := sfnClient.StartExecution(ctx, &sfn.StartExecutionInput{
+			StateMachineArn: aws.String(stateMachineArn),
+			Input:           aws.String(fmt.Sprintf("{\"orderId\": \"%s\"}", orderId)),
+		})
+		if err != nil {
+			t.Fatal(fmt.Errorf("Could not start execution: %w", err))
+		}
+		executionArn := out.ExecutionArn
+
+		fmt.Println("Order ordered! checking in the database")
+
+		err = retry.Do(func() error {
+			order, err := readOrder(ctx, dynamoDBClient, orderId)
+			fmt.Println("Checking for ordered order", order, err)
+
+			if err != nil {
+				return err
+			}
+			if order.Status != "ORDERED" {
+				return fmt.Errorf("Order is not ordered: %s", order.Status)
+			}
+			return nil
+		}, retry.Attempts(5), retry.Delay(500*time.Millisecond))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		for currentOrderedOrders := range orderedOrders {
+			for _, orderedOrder := range currentOrderedOrders {
+				if orderedOrder.OrderId != orderId {
+					continue
+				}
+
+				_, err = sfnClient.SendTaskSuccess(ctx, &sfn.SendTaskSuccessInput{
+					Output: aws.String(
+						fmt.Sprintf("{\"orderId\": \"%s\", \"accepted\": false}", orderedOrder.OrderId),
+					),
+					TaskToken: aws.String(orderedOrder.TaskToken),
+				})
+				if err != nil {
+					t.Fatal(fmt.Errorf("Could not send task success: %w", err))
+				}
+
+				stopPollingForOrderedOrders()
+			}
+		}
+
+		fmt.Println("Order rejected! checking in the database")
+
+		err = retry.Do(func() error {
+			order, err := readOrder(ctx, dynamoDBClient, orderId)
+			fmt.Println("Checking for rejected orders", order, err)
+			if err != nil {
+				return err
+			}
+			if order.Status != "REJECTED" {
+				return fmt.Errorf("Order is not rejected: %s", order.Status)
+			}
+
+			return nil
+		}, retry.Attempts(5), retry.Delay(500*time.Millisecond))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Println("Order rejected in the database! Checking notifications")
+
+		for currentRejectedOrders := range rejectedOrders {
+			fmt.Println("Current rejected orders", currentRejectedOrders)
+
+			for _, rejectedOrder := range currentRejectedOrders {
+				if rejectedOrder.OrderId != orderId {
+					continue
+				}
+
+				stopPollingForRejectedOrders()
+			}
+		}
+
+		fmt.Println("User notified about the rejection. Checking execution status")
+
+		err = retry.Do(func() error {
+			out, err := sfnClient.DescribeExecution(ctx, &sfn.DescribeExecutionInput{
+				ExecutionArn: aws.String(*executionArn),
+			})
+			if err != nil {
+				return fmt.Errorf("Could not describe execution: %w", err)
+			}
+
+			if out.Status != sfntypes.ExecutionStatusSucceeded {
+				return fmt.Errorf("Execution is not succeeded: %s", out.Status)
+			}
+
+			return nil
+		}, retry.Attempts(5), retry.Delay(1*time.Second))
+		if err != nil {
+			t.Fatal(fmt.Errorf("Could not describe execution: %w", err))
+		}
 
 	})
 }
@@ -182,8 +339,36 @@ func pollForOrderMessages(ctx context.Context, queueUrl string) (func(), chan []
 
 	return func() {
 		ticker.Stop()
+
 		close(quit)
 		close(results)
 	}, results
 
+}
+
+type Order struct {
+	OrderId string `json:"orderId" dynamodbav:"orderId"`
+	Status  string `json:"status" dynamodbav:"status"`
+}
+
+func readOrder(ctx context.Context, client *dynamodb.Client, orderId string) (Order, error) {
+	var order Order
+	out, err := client.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: aws.String(os.Getenv("OrdersTableName")),
+		Key: map[string]dynamodbtypes.AttributeValue{
+			"orderId": &dynamodbtypes.AttributeValueMemberS{
+				Value: orderId,
+			},
+		},
+	})
+	if err != nil {
+		return order, err
+	}
+
+	err = dynamodbattributevalue.UnmarshalMap(out.Item, &order)
+	if err != nil {
+		return order, err
+	}
+
+	return order, nil
 }
--- testing-sfn/complex-machine/go.mod
+++ testing-sfn/complex-machine/go.mod
@@ -7,10 +7,15 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.17.3 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.18.7 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.13.7 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.10.8 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.21 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.27 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.21 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.17.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.13.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.21 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.21 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sfn v1.16.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.19.17 // indirect
@@ -19,5 +24,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.17.7 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 )
